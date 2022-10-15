e77ffa367ea6ee308111a11bd17c2cb588a68931
John <user@dhcp-10-250-205-128.harvard.edu>
2022-10-15 11:13:52 -0400

    mainpage

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Box, Button, Flex, Image, Spacer } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Box, Button, Flex, Image, Spacer, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { SearchIcon } from '@chakra-ui/icons'
+import { Link } from "react-router-dom";
 
-import './Nav.css'
+import "./Nav.css";
 import Logo from "./assets/images/Logo.png";
 
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
 //import AcceptedNFT from './AcceptedNFT.json';
 
@@ -16,83 +17,143 @@ import { ethers } from 'ethers';
 //const firestore = firebase.firestore();
 
 const Nav = ({ accounts, setAccounts }) => {
-    const isConnected = Boolean(accounts[0]);
+  const [value, setValue] = React.useState("");
+  const handleChange = (event) => setValue(event.target.value);
 
-    async function connectAccount() {
-        if (window.ethereum) {
-            const accounts = await window.ethereum.request({
-                method: "eth_requestAccounts",
-            });
-            setAccounts(accounts);
+  const isConnected = Boolean(accounts[0]);
 
+  async function connectAccount() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccounts(accounts);
 
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = provider.getSigner();
-            /*
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      /*
             const contract = new ethers.Contract(
                 acceptedNFTAddress,
                 AcceptedNFT.abi,
                 signer
             );
             */
-
-        }
     }
+  }
 
-    return (
-        <div className="Nav">
-            <Flex justify="space-between" align="center" backgroundColor="rgba(0,0,0,0.9)" height="90px">
-                {/* left side - social media icons */}
-                
-                <Box marginLeft="60px">
-                <a href="" rel="noreferrer">
-                    <Image src={Logo} boxSize="42px" marginLeft="25px"/>
-                    SafePort
-                </a>
-                </Box>
+  return (
+    <div className="Nav">
+      <Flex
+        justify="space-between"
+        align="center"
+        backgroundColor="rgba(0,0,0,0.9)"
+        height="90px"
+      >
+        {/* left side - social media icons */}
 
-                {/* right side - website menu */}
-                <Flex justify="space-around" align="center" width="55%" padding="30px">
-                    <Box margin="0 15px">
-                        <Link to="/" style={{ textDecoration: 'none', color: 'white', fontSize: '25px' }} >Home</Link>
-                    </Box>
-                    <Spacer />
-                    <Box margin="0 15px">
-                        <a href="https://rinkebyfaucet.com/" target="_blank" style={{ textDecoration: 'none', color: 'white', fontSize: '25px' }} rel="noreferrer">Faucet</a>
-                    </Box>
-                    <Spacer />
-                    <Box margin="0 15px">
-                        <Link to="/" style={{ textDecoration: 'none', color: 'white', fontSize: '25px' }} >About</Link>
-                    </Box>
-                    <Spacer />
-                    
-                    {/* connect */}
-                    {isConnected ? (
-                        <Box backgroundColor="green"
-                            fontFamily="inherit" borderRadius="5px"
-                            width="150px" fontSize="20px" color="white"
-                            padding="12px" margin="0 15px"
-                            boxShadow="0px 2px 2px 1px #0F0F0F" textAlign="center">
-                            Connected
-                        </Box>
-                    ) : (
-                        <Button
-                            backgroundColor="#D6517D" borderRadius="5px"
-                            boxShadow="0px 2px 2px 1px #0F0F0F"
-                            color="white" cursor="pointer"
-                            fontFamily="inherit" fontSize="20px"
-                            width="150px" padding="12px" height="50px"
-                            margin="0 15px"
-                            onClick={() => { connectAccount(); }}>
-                            Connect
-                        </Button>
-                    )}
+        <Box marginLeft="60px">
+          <a href="" rel="noreferrer">
+            <Image src={Logo} boxSize="42px" marginLeft="25px" />
+            SafePort
+          </a>
+        </Box>
+        <InputGroup marginLeft="60px">
+          <InputLeftElement
+            pointerEvents="none"
+            children={<SearchIcon color="gray.300" />}
+          />
+          <Input
+            value={value}
+            onChange={handleChange}
+            placeholder="Search Items, Collections, and Wallets"
+            width="60%"
+            size="md"
+            borderRadius="20px"
+          />
+        </InputGroup>
 
-                </Flex>
+        {/* right side - website menu */}
+        <Flex justify="space-around" align="center" width="55%" padding="30px">
+          <Box margin="0 15px">
+            <Link
+              to="/"
+              style={{
+                textDecoration: "none",
+                color: "white",
+                fontSize: "25px",
+              }}
+            >
+              Register
+            </Link>
+          </Box>
+          <Spacer />
+          <Box margin="0 15px">
+          <Link
+              to="/"
+              style={{
+                textDecoration: "none",
+                color: "white",
+                fontSize: "25px",
+              }}
+            >
+              Claim
+            </Link>
+          </Box>
+          <Spacer />
+          <Box margin="0 15px">
+            <Link
+              to="/"
+              style={{
+                textDecoration: "none",
+                color: "white",
+                fontSize: "25px",
+              }}
+            >
+              Explore
+            </Link>
+          </Box>
+          <Spacer />
 
-            </Flex>
-        </div>
-    );
+          {/* connect */}
+          {isConnected ? (
+            <Box
+              backgroundColor="green"
+              fontFamily="inherit"
+              borderRadius="5px"
+              width="150px"
+              fontSize="20px"
+              color="white"
+              padding="12px"
+              margin="0 15px"
+              boxShadow="0px 2px 2px 1px #0F0F0F"
+              textAlign="center"
+            >
+              Connected
+            </Box>
+          ) : (
+            <Button
+              backgroundColor="#D6517D"
+              borderRadius="5px"
+              boxShadow="0px 2px 2px 1px #0F0F0F"
+              color="white"
+              cursor="pointer"
+              fontFamily="inherit"
+              fontSize="18px"
+              width="150px"
+              padding="12px"
+              height="50px"
+              margin="0 15px"
+              onClick={() => {
+                connectAccount();
+              }}
+            >
+              Connect Wallet
+            </Button>
+          )}
+        </Flex>
+      </Flex>
+    </div>
+  );
 };
 
 export default Nav;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,34 +1,74 @@
-import { Box, Button, Flex, Text, Center, Spacer, Input, Image } from '@chakra-ui/react';
-import { ethers, BigNumber } from 'ethers';
+import { Box, Button, Flex, Text, Center, Spacer, Input, Image } from "@chakra-ui/react";
+import { ethers, BigNumber } from "ethers";
 
-import './Main.css';
+import "./Main.css";
 
-function Main(props) {
-    const { navibarProps } = props;
-  
+const Main = ({ accounts, setAccounts }) => {
+    const isConnected = Boolean(accounts[0]);
+
     return (
-        <Flex justify="center" align="center" width="100%" flexDirection="column">
-        <Flex bg='rgba(0,0,0,0.2)' borderRadius="30px" w='70%' marginTop="170px" paddingBottom="30px" height="300px" flexDirection="column" justify="center">
-            <Text fontSize="50px" fontFamily="Ubuntu" textShadow="0 3px #000000">SafePort</Text>
-            <Text
-                fontSize="40px"
-                letterSpacing="-5.5%"
-                fontFamily="Ubuntu"
-                textShadow="0 2px 2px #000000"
-                color='tomato'
-            >
-                'your stuff 2 nft'</Text>
-            <Text
-                fontSize="30px"
-                letterSpacing="-5.5%"
-                fontFamily="Ubuntu"
-                textShadow="0 2px 2px #000000"
-            >
-                We safely store you stuffz
-            </Text>
-        </Flex>
-        </Flex>
+
+        <div className="overlay">
+            <Flex justify="center" align="center" width="100%" flexDirection="column">
+                <Flex
+                    bg="rgba(0,0,0,0.5)"
+                    borderRadius="30px"
+                    w="70%"
+                    marginTop="70px"
+                    marginBottom="70px"
+                    paddingBottom="30px"
+                    height="500px"
+                    flexDirection="column"
+                    justify="center"
+                >
+                    <Text fontSize="50px" fontFamily="Ubuntu" textShadow="0 3px #000000">
+                        SafePort
+                    </Text>
+                    <Text
+                        fontSize="40px"
+                        letterSpacing="-5.5%"
+                        fontFamily="Ubuntu"
+                        textShadow="0 2px 2px #000000"
+                        color="tomato"
+                    >
+                        Store your physical items and luxury goods as an NFT
+                    </Text>
+
+                    <Text
+                        fontSize="30px"
+                        letterSpacing="-5.5%"
+                        fontFamily="Ubuntu"
+                        textShadow="0 2px 2px #000000"
+                        marginTop="50px"
+                    >
+                        How it works
+                    </Text>
+                    <Text
+                        fontSize="30px"
+                        letterSpacing="-5.5%"
+                        textShadow="0 2px 2px #000000"
+                    >
+                        1. Register your item on our site
+                    </Text>
+                    <Text
+                        fontSize="30px"
+                        letterSpacing="-5.5%"
+                        textShadow="0 2px 2px #000000"
+                    >
+                        2. Send your physical item to our warehouse freeport
+                    </Text>
+                    <Text
+                        fontSize="30px"
+                        letterSpacing="-5.5%"
+                        textShadow="0 2px 2px #000000"
+                    >
+                        3. Upon receipt of the item, you will be airdropped an NFT which you
+                        can freely trade, auction, or fractionalize
+                    </Text>
+                </Flex>
+            </Flex>
+        </div>
     );
-  }
-  
-  export default Main;
+};
+
+export default Main;
