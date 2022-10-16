41120bff88e8e222f7ae1cce473c712714d628d1
DK <dongkyuk@andrew.cmu.edu>
2022-10-15 22:21:40 -0400

    add museum toast

--- src/pages/ParisMuseum.js
+++ src/pages/ParisMuseum.js
@@ -7,18 +7,19 @@ import {
   Select,
   HStack,
   Button,
+  useToast,
 } from "@chakra-ui/react";
 import { ethers, BigNumber } from "ethers";
 
 const ParisMuseum = ({ accounts, setAccounts }) => {
   const isConnected = Boolean(accounts[0]);
-
+  const toast = useToast();
   return (
     <Center>
       <VStack spacing="30px">
         <Box marginTop="80px" />
         <Text textColor="black">
-          <b>Paris Museum</b>
+          <b>Boston Wine Collection</b>
         </Text>
         <HStack spacing="30px">
           <Image
@@ -26,12 +27,25 @@ const ParisMuseum = ({ accounts, setAccounts }) => {
             alt="Wine"
           />
           <VStack spacing="30px">
-            <Select placeholder="Add your collection" textColor="black">
+            <Select placeholder="Request addition of your collection" textColor="black">
               <option value="option1">Option 1</option>
               <option value="option2">Option 2</option>
               <option value="option3">Option 3</option>
             </Select>
-            <Button colorScheme="blue">Add to Museum</Button>
+            <Button
+              colorScheme="blue"
+              onClick={() =>
+                toast({
+                  title: "Your collection has been requested for addition.",
+                  description: "We will review the request and get back to you.",
+                  status: "success",
+                  duration: 9000,
+                  isClosable: true,
+                })
+              }
+            >
+              Add to Museum
+            </Button>
           </VStack>
         </HStack>
       </VStack>
